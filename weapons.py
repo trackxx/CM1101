3d588a366338cdcbd57b66ef0bd73ade769bc5f2
trackxx <jslawinski95@gmail.com>
2015-10-20 18:54:09 +0100

    Update weapons.py

--- weapons.py
+++ weapons.py
@@ -2,7 +2,7 @@ weapon_fists = {
     "id": "fist",
     "name": "Fists",
     "damage": 5,
-    "description": """Fists""",
+    "description": """Those are your fists.""",
     "illegal": False
 }
 
@@ -10,7 +10,7 @@ weapon_big_fists = {
     "id": "big_fist",
     "name": "Fists",
     "damage": 10,
-    "description": """Fists""",
+    "description": """These are your fists, but you find them more powerful than before.""",
     "illegal": False
 }
 
@@ -18,7 +18,7 @@ weapon_giant_fists = {
     "id": "giant_fist",
     "name": "Fists",
     "damage": 15,
-    "description": """Fists""",
+    "description": """These are your fists, but, woah, mate... They sure can knock someone unconcious""",
     "illegal": False
 }
 
@@ -26,7 +26,7 @@ weapon_foot = {
     "id": "foot",
     "name": "Foot",
     "damage": 7,
-    "description": """Foot""",
+    "description": """This is your foot. It's kind of smelly, you could use a bath.""",
     "illegal": False
 }
 
@@ -34,7 +34,7 @@ weapon_bottle = {
     "id": "bottle",
     "name": "a Champagne Bottle",
     "damage": 100,
-    "description": """DESCRIPTION""",
+    "description": """A £2 bottle of russian Champagne. It's so tasteless, someone should make it illegal."",
     "illegal": True
 }
 
@@ -42,7 +42,7 @@ weapon_bucket = {
     "id": "bucket",
     "name": "an Ice Bucket",
     "damage": 25,
-    "description": """DESCRIPTION""",
+    "description": """Something used in challenge so outdated, that Moses finds it old and not entertaining.""",
     "illegal": False
 }
 
@@ -50,7 +50,7 @@ weapon_spatula = {
     "id": "spatula",
     "name": "a Spatula",
     "damage": 10,
-    "description": """DECRIPTION""",
+    "description": """Are you gonna fight someone with a SPATULA? Seriously, get a proper weapon. Like a hotdog.""",
     "illegal": False
 }
 
@@ -58,7 +58,7 @@ weapon_hotdog = {
     "id": "hotdog",
     "name": "a Hotdog",
     "damage": 8,
-    "description": """DECRIPTION""",
+    "description": """'Mustard, ketchup, anything else, love?' If you never fought with a hotdog, now it's time to prove fastfood lovers, that junky food is dangerous!""",
     "illegal": False
 }
 
@@ -66,7 +66,7 @@ weapon_glass = {
     "id": "glass",
     "name": "an Empty Glass Bottle",
     "damage": 15,
-    "description": """DECRIPTION""",
+    "description": """Oughh, Champagne is gone, but the spirit smell isn't. Don't make the mistake person that drank it did.""",
     "illegal": True
 }
 
@@ -74,7 +74,7 @@ weapon_inflatable = {
     "id": "inflatable",
     "name": "an Inflatable Object",
     "damage": 15,
-    "description": """DECRIPTION""",
+    "description": """Wait... Is that... An inflatable penis?! Who even buys those things? Not a best kind of a weapon.""",
     "illegal": False
 }
 
@@ -82,7 +82,7 @@ weapon_lollipop = {
     "id": "lollipop",
     "name": "a Lollipop",
     "damage": 2,
-    "description": """DECRIPTION""",
+    "description": """Mmmm... I can smell strawberry!""",
     "illegal": False
 }
 
@@ -90,7 +90,7 @@ weapon_lighter = {
     "id": "lighter",
     "name": "a Lighter",
     "damage": 8,
-    "description": """DECRIPTION""",
+    "description": """Regular lighter. If you don't have a gasoline canister with you, not much of a weapon.""",
     "illegal": False
 }
 
@@ -98,6 +98,6 @@ weapon_cup = {
     "id": "cup",
     "name": "a Plastic Cup",
     "damage": 10,
-    "description": """DECRIPTION""",
+    "description": """Live by a plastic cup, die by a plastic cup.""",
     "illegal": False
 }
