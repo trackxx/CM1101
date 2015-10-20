2435ddf4d924ebbe9d694a5ae502031d013d860b
mlaw54 <michael.lawson@hotmail.co.uk>
2015-10-20 15:41:20 +0100

    Update map.py

--- map.py
+++ map.py
@@ -1,72 +1,243 @@
 from items import *
-from enemy import *
+from people import *
 
-room_factory = {
-    "name": "Factory Floor",
-
-    "description":
-    """You are standing in the middle of the cheese factory floor. You see a cheese grater
-    and a block of cheese """,
-
-    "exits": {"south": "Staff Room", "east": "Locker room", "west": "Parking"},
-
-    "items": [item_cheese_grater, item_cheese],
-
-    "enemy": enemy_one
-}
-
-room_staff = {
-    "name": "Staff Room",
+room_entrance = {
+    "name": "Entrance",
 
     "description":
-    """This room is where you spent many lunch breaks opening
-    up the fridge to find someone has already eatten your sandwitches and replaces
-    it with a sticker""",
+    """ """,
 
-    "exits": {"north": "Factory Floor"},
+    "exits": {"west": "Exit", "east" : "Rnb Room", "north" : "Stairs to first floor"},
 
-    "items": [item_sticker, item_id]
+    "items": [],
+
+    "people": [person_bouncer]
 }
 
-room_locker = {
-    "name": "Locker room",
+room_rnb_bar = {
+    "name": " Rnb Bar",
 
     "description":
-    """A room packed with lockers. to your surprise there is a note on your locker
-    """,
+    """ """,
 
-    "exits": {"west": "Factory Floor"},
+    "exits": {"west": "Rnb Room"},
 
-    "items": [item_wallet, item_mobile, item_note]
+    "items": [],
+
+    "people": [person_toilet_guy]
+
+   
+
+    
 }
 
-room_parking = {
-    "name": "the parking lot",
+room_Rnb = {
+    "name": "Rnb Room",
 
     "description":
-    """You are standing in a car park remembering that you dont actually own a car
-    a scooter to work every day. Your scooter is up against the wall.""",
+    """ """,
 
-    "exits": {"east": "Office", "south": "Factory Floor"},
+    "exits": {"west" : "Entrance", "east" : "Rnb Bar"},
 
-    "items": [item_scooter]
+    "items": [item_phone],
+
+
+
+   
 }
 
-room_office = {
-    "name": "Managers Office",
+room_cloak = {
+    "name": "Cloak room",
 
     "description":
-    """In this room lives the guy who has made your life a living hell for the past 10 years.""",
+    """ """,
 
-    "exits": {"west": "Parking"},
+    "exits": {"east" : "Stairs to first floor "},
 
-    "items": []
+    "items": [item_keys,item_card],
+    
+    "people": [person_cloakroom]
+   
 }
+room_stairs1 = {
+    "name": "Stairs to first floor",
+
+    "description":
+    """ """,
+
+    "exits": {"north" : "Stairs to second floor", "south" : "Entrance", "east" : "Hotdog stand"},
+
+    "items": [],
+
+    
+        }
+room_hotdog = {
+    "name": "Hotdog stand",
+
+    "description":
+    """ """,
+
+    "exits": {"east": "Toilet", "west" : "Stairs to first floor" },
+
+    "items": [item_wallet,item_hotdog,item_hotdog_stand],
+
+    "people": [person_hotdog_guy,person_smoking_area_guy ]
+    
+        }
+room_toilet1 = {
+    "name": "Toilet",
+
+    "description":
+    """ """,
+
+    "exits": {"west" : "Hotdog stand"},
+
+    "items": [item_money],
+
+     "people": [person_toilet_guy]
+
+     
+        }
+        
+room_toilet2 = {
+    "name": "Toilet2",
+
+    "description":
+    """ """,
+
+    "exits": {"west" : "Hotdog stand"},
+
+    "items": [item_cologne],
+
+  #  "enemy": 
+        }
+room_stairs2 = {
+    "name": "Stairs to second floor ",
+
+    "description":
+    """ """,
+
+    "exits": {"west" :  "Vip Room", "south" : "Stairs to first floor", "east" :"Pryzm Room", "north" : "Stairs to third floor" },
+
+    "items": [],
+
+   
+        }
+room_stairs3 = {
+    "name": "Stairs to third floor ",
+
+    "description":
+    """ """,
+
+    "exits": {"north" : "Disco Room", "south" : "Stairs to second floor" },
+
+    "items": [],
+}
+room_vip = {
+    "name": "Vip Room",
+
+    "description" :
+    """ """,
+
+    "exits": {"east" : "Stairs to second floor"},
+
+    "items": [item_student_card,item_table],
+
+  "people": [person_housemate_girl,person_housemate_guy] 
+        }
+room_Main = {
+    "name": "Pryzm Room",
+
+    "description":
+    """ """,
+
+    "exits": {"east" : "Pryzm room Bar", "west" : "Stairs to second floor" },
+
+    "items": [item_water],
+
+
+        }
+room_main_bar = {
+    "name": "Pryzm room Bar",
+
+    "description":
+    """ """,
+
+    "exits": {"west" :"Pryzm Room" ,"south" : "Smoking area"},
+
+    "items": [item_id],
+
+  "people": [person_coursemate_girl,person_coursemate_boyfriend,person_barman_one] 
+        }
+room_smoking = {
+    "name": "Smoking area",
+
+    "description":
+    """ """,
+
+    "exits": {"north" : "Pryzam room Bar"},
+
+    "items": [item_wristband],
+
+  "people":[person_smoking_area_guy]
+        }
+room_disco = {
+    "name": "Disco Room",
+
+    "description":
+    """ """,
+
+    "exits": {"east" : "Disco room bar", "south" : "Stairs to second floor"},
+
+    "items": [item_number,item_ticket ],
+
+  "people":[person_rugby_guy]
+        }
+room_disco_bar = {
+    "name": "Disco room bar",
+
+    "description":
+    """ """,
+
+    "exits": {"west" : "Disco Room"},
+
+    "items": [item_jager,item_vodka],
+
+  "people":[person_hot_girl, person_barman_two ]  
+        }
+room_exit = {
+    "name": "exit",
+
+    "description":
+    """ """,
+
+    "exits": {""},
+
+    "items": [],
+
+   "people":{" "}
+        }
+
+
+
 
 rooms = {
-    "Factory Floor": room_factory,
-    "Staff Room": room_staff,
-    "Locker room": room_locker,
-    "Parking": room_parking,
-    "Office": room_office
+    "Entrance": room_entrance,
+    "Rnb Bar": room_rnb_bar,
+    "Rnb Room": room_Rnb,
+    "Cloak room": room_cloak,
+    "Stairs to first floor": room_stairs1,
+    "Hotdog stand": room_hotdog,
+    "Toilet": room_toilet1,
+    "Toilet2": room_toilet2,
+    "Stairs to second floor": room_stairs2,
+    "Vip Room": room_vip,
+    "Pryzm Room": room_Main,
+    "Pryzm room Bar": room_main_bar,
+    "Smoking area": room_smoking,
+    "Disco Room": room_disco,
+    "Disco room bar": room_disco_bar,
+    "Exit": room_exit,
+     "Stairs to third floor" : room_stairs3,
+
 }
